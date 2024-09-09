47660f1bf783fcef8c9a110fdf441a6b5046b130
Shrilaxmi-16 <131773356+Shrilaxmi-16@users.noreply.github.com>
2024-09-10 00:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info('This App is for machine learning analysis')
-df= pd.read_csv('https://raw.githubusercontent.com/sumukhahe/ML_Project/main/data/dataset.csv')
-df
+with st.expander('Data'):
+  st.write('## Dataset')
+  df= pd.read_csv('https://raw.githubusercontent.com/sumukhahe/ML_Project/main/data/dataset.csv')
+  df
