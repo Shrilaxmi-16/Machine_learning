78b1e43e4a9a6687971326b535fc647c3883297f
Shrilaxmi-16 <131773356+Shrilaxmi-16@users.noreply.github.com>
2024-09-10 02:30:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-import matplotlib.pyplot as plt
+
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
