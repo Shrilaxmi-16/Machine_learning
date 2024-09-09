e28a4e53db8953bf4c10f056147492712d896175
Shrilaxmi-16 <131773356+Shrilaxmi-16@users.noreply.github.com>
2024-09-10 01:03:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ with st.expander('Data'):
   df
 with st.expander('Data Visualization'):
   st.write('## Crop Production per Year')
-  st.scatter_chart(data=df, *, x='year', y='Production_(in_Tonnes)', x_label='Year', y_label='Production', color='crop')
+  st.scatter_chart(data=df, x='year', y='Production_(in_Tonnes)', x_label='Year', y_label='Production', color='crop')
 
